9e957387c29e5a8fa11c8b8da44989a5fc011c53
DOliveraAntonio <140433598+DOliveraAntonio@users.noreply.github.com>
2024-11-13 09:43:43 -0500

    UPDATE Detalle tour servicios

--- 2024/foreign_key/tour_servicios/fks_all.sql
+++ 2024/foreign_key/tour_servicios/fks_all.sql
@@ -5,26 +5,20 @@ alter table tour_servicios
 -- ############################################################################ TOUR
 -- CAMBIAR VALOR A NULL
 UPDATE tour_servicios
-SET "user" = NULL
-where "user" = 0;
-
--- AQUI SOLO CONSULTAMOS LOS REGISTROS A ELIMINAR
-SELECT * FROM tour_servicios
-WHERE "user" NOT IN (
-    SELECT id FROM users
-);
+SET id_tour = NULL
+where id_tour = 0;
 
 -- AQUI YA LOS QUERYS PARA ELIMINAR ESOS REGISTROS
 DELETE FROM tour_servicios
-WHERE "user" NOT IN (
-    SELECT id FROM users
+WHERE id_tour NOT IN (
+    SELECT id FROM tour
 );
 
 -- YA PODEMOS CREAR LAS LLAVES FORANEAS
 ALTER TABLE tour_servicios
-ADD CONSTRAINT users_fk
-FOREIGN KEY ("user")
-REFERENCES users (id)
+ADD CONSTRAINT tour_fk
+FOREIGN KEY (id_tour)
+REFERENCES tour (id)
 ON DELETE CASCADE ;
 
 -- ############################################################################ USERS
